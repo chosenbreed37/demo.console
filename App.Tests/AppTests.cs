a0ddcfac9a4259ebd5d9ac4bbc3d9d74d27fcfcc
chosenbreed37 <chosenbreed@gmail.com>
2016-07-04 16:24:04 +0100

    Added app and app.test

---
+++ App.Tests/AppTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Tests
+{
+    [TestFixture]
+    public class AppTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+        }
+    }
+}
