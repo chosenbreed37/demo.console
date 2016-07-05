696f215b24696b8d7ea3fbff79a2ccfbc2adf96d
chosenbreed37 <chosenbreed@gmail.com>
2016-07-05 16:08:44 +0100

    Correcting paths for nuget restore...

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 REM restore packages
-.nuget/nuget.exe install App.Tests/packages.config -OutputDirectory packages
+.nuget\nuget.exe install App.Tests\packages.config -OutputDirectory packages
 
 REM build
 msbuild demo.console.sln
