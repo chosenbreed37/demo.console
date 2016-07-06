1a3d91e4a61eafa04b9b1a01360144831c063397
chosenbreed37 <chosenbreed@gmail.com>
2016-07-06 09:39:03 +0100

    Tweaking tools

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 REM restore packages
-.nuget\nuget.exe install App.Tests\packages.config -OutputDirectory packages
+tools\nuget\nuget.exe install App.Tests\packages.config -OutputDirectory packages
 
 REM build
 msbuild demo.console.sln
 
 REM run tests
-packages\NUnit.ConsoleRunner.3.4.11\tools\nunit3-console.exe App.Tests\bin\debug\App.Tests.dll
+tools\NUnit.ConsoleRunner.3.4.1\nunit3-console.exe App.Tests\bin\debug\App.Tests.dll
 
