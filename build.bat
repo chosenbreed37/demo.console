ea67f96885d7415378bc2bb0df666eaa199b5c45
chosenbreed37 <chosenbreed@gmail.com>
2016-07-07 17:06:47 +0100

    Tweaking build file

--- build.bat
+++ build.bat
@@ -5,5 +5,6 @@ REM build
 msbuild demo.console.sln /t:Rebuild /p:Configuration=Release
 
 REM run tests
-tools\NUnit.ConsoleRunner.3.4.1\nunit3-console.exe App.Tests\bin\debug\App.Tests.dll
+tools\NUnit.ConsoleRunner.3.4.1\nunit3-console.exe App.Tests\bin\Release\App.Tests.dll --output=nunit-result.xml
+
 
