6c4a246f24c1206a2c47e3de371525260985217b
chosenbreed37 <chosenbreed@gmail.com>
2016-07-07 15:08:08 +0100

    Tweaking build file...

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ REM restore packages
 tools\nuget\nuget.exe install App.Tests\packages.config -OutputDirectory packages
 
 REM build
-msbuild demo.console.sln
+msbuild demo.console.sln /t:Rebuild /p:Configuration=Release
 
 REM run tests
 tools\NUnit.ConsoleRunner.3.4.1\nunit3-console.exe App.Tests\bin\debug\App.Tests.dll
