a8b036bffccea289902013ae2996030a5b62e196
chosenbreed37 <chosenbreed@gmail.com>
2016-07-08 09:50:25 +0100

    Tweaking test execution to use nunit2 format

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 REM restore packages
+tools\nuget\nuget.exe install App\packages.config -OutputDirectory packages
 tools\nuget\nuget.exe install App.Tests\packages.config -OutputDirectory packages
 
 REM build
 msbuild demo.console.sln /t:Rebuild /p:Configuration=Release
 
 REM run tests
-tools\NUnit.ConsoleRunner.3.4.1\nunit3-console.exe App.Tests\bin\Release\App.Tests.dll --output=nunit-result.xml
-
+packages\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe App.Tests\bin\Release\App.Tests.dll --result:result.xml;format=nunit2
 
